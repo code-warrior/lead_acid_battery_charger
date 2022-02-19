b0cc0a8d1edf29a40c34e30c1cd6ea875e12f308
Roy Vanegas <roy@thecodeeducators.com>
2022-02-19 07:08:44 -0500

    Organize constants

--- constants.h
+++ constants.h
@@ -1,8 +1,9 @@
-#define ONE_SECOND 1000
-
 // Pins
 #define RELAY_PIN                 5
 #define INCREMENT_CAPACITY_BUTTON 4
 #define START_CHARGING_BUTTON     3
 #define ACS712_CURRENT_SENSOR     A0
+
+// Misc
 #define MAX_HOURS_BATTERY_SHOULD_CHARGE 7
+#define ONE_SECOND                      1000
